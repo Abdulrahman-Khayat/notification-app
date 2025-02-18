00fd3599148c2489dbd6c2886b1132c6efc30925
Abdulrahman Khayat <arahman.khayat@gmail.com>
2025-02-18 18:07:42 +0300

    add notification class in shared library

---
+++ NotificationApp/Common/Notification.cs
@@ -0,0 +1,17 @@
+﻿namespace Common;
+
+public class Notification
+{
+    public Guid EventId { get; set; } = Guid.NewGuid();
+    public string EventType { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+    public NotificationData Data { get; set; }
+}
+public class NotificationData
+{
+    public string Type { get; set; }  // "email", "sms", "push"
+    public string Recipient { get; set; }
+    public Dictionary<string, string> Variables { get; set; }
+}
