b6806d4fdeb89b7f7068d64737c863fc675aa6b5
Abdulrahman Khayat <arahman.khayat@gmail.com>
2025-02-18 18:08:02 +0300

    add message-broker config in userService

--- NotificationApp/UserService/Program.cs
+++ NotificationApp/UserService/Program.cs
@@ -1,6 +1,40 @@
+using System.Reflection;
+using Common;
+using MassTransit;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UserService.Data;
+using UserService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUserRepo, UserRepo>();
+builder.Services.AddScoped<PasswordHasher<User>>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context,cfg) =>
+    {
+        cfg.Host("localhost", "/", h => {
+            h.Username("guest");
+            h.Password("guest");
+        });
+        cfg.Message<Notification>(m => m.SetEntityName("user-events"));
+        cfg.Publish<Notification>(p => p.ExchangeType = "topic");
+
+        cfg.ConfigureEndpoints(context);
+    }); 
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
